afaafeae6fba2c4eca43f629a030d988fd855ba2
Tyler Swartz <tgswartz@gmail.com>
2014-03-08 13:07:55 -0800

    set up game play

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -3,15 +3,80 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+	def calculate_total(cards)
+		arr = cards.map{|element| element[1]}
+
+		total = 0
+		arr.each do |a|
+			if a == "A"
+				total += 11
+			else
+				total += a.to_i == 0 ? 10 : a.to_i
+			end
+		end
+
+		arr.select{|element| element == "A"}.count.times do
+			break if total <= 21
+			total -= 10
+		end
+
+		total
+	end
+end
+
+before do
+	@show_hit_or_stay_buttons = true
+end
 
 get '/' do
-	"Hello Jody!"
+	if session[:player_name]
+		redirect '/game'
+	else
+		redirect '/new_player'
+	end	
 end
 
-get '/test' do
-	erb :test_template
+get '/new_player' do
+	erb :new_player
 end
 
-get '/nested' do
-	erb :"/Nested/nested_template"
+post '/new_player' do
+	session[:player_name] = params[:player_name]
+	redirect '/game'
 end
+
+get '/game' do
+	suits = ['H','D','C','S']
+	values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+	session[:deck] = suits.product(values).shuffle! # will produce ['H','9']
+	
+	session[:dealer_cards] = []
+	session[:player_cards] = []
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop	
+
+	erb :game
+end
+
+post '/game/player/hit' do
+	session[:player_cards] << session[:deck].pop
+	if calculate_total(session[:player_cards]) > 21
+		@error = "Sorry, you busted."
+		@show_hit_or_stay_buttons = false
+	end
+
+	erb :game
+end
+
+post '/game/player/stay' do
+	@success = "You have chosen to stay."
+	@show_hit_or_stay_buttons = false
+	erb :game
+end
+
+
+
+
